e29ff7d9dee58f2a61f8c85be861c3d8e4fd61f1
Siddhant Misra <misrasiddhant@yahoo.co.in>
2019-01-12 19:13:44 +0000

    LoadDataType2.sh updated

--- shell/HiveLoadAsTypeII/LoadDataType2.sh
+++ shell/HiveLoadAsTypeII/LoadDataType2.sh
@@ -1,9 +1,23 @@
-SOURCE="S"
-TARGET="T"
-PRIMARY_KEY="P"
-FORMAT="F"
+ValidateReturnCode(){
+	CODE=$1
+	echo "CODE $CODE"
+	IF [[ "$CODE" == "0" ]];THEN
+		echo "Successful Execution"
+		return 0
+	ELSE
+		echo "Execution Error"
+		return 1
+	FI
+}
 
-while getopts s:t:p:h: option
+PrintLog(){
+	echo $#
+	LOG_TYPE=$1
+	LOG_MSG=$2
+	echo "$LOG_TYPE - $LOG_MSG"
+}
+
+while getopts 's:t:p:h:' option
 do
 	case "${option}" in
 		s) 
@@ -15,7 +29,7 @@ do
 		p)
 			PRIMARY_KEY=${OPTARG}
 			;;
-		h)
+		help)
 			FORMAT=${OPTARG}
 			;;
 	esac
@@ -26,3 +40,20 @@ echo $SOURCE
 echo $TARGET
 echo $PRIMARY_KEY
 echo $FORMAT
+
+PrintLog "INFO" "Fetching Columns from src tbl $SOURCE"
+SRC_COLUMNS=`hive -e "SHOW COLUMNS IN $SOURCE"`
+ValidateReturnCode $?
+if [[ $? == "0" ]];then
+	echo $SRC_COLUMNS
+fi
+
+
+
+
+PrintLog "INFO" "Fetching Columns from tgt tbl $TARGET"
+TGT_COLUMNS=`hive -e "SHOW COLUMNS IN $TARGET"`
+ValidateReturnCode $?
+if [[ $? == "0" ]];then
+	echo $TGT_COLUMNS
+fi
